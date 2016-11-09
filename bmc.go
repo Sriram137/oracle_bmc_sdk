0dd7d2e817d87eb7603f00dd1ccc10bf32fe9074
Sriram Gansesan <sriramg@qubole.com>
2016-11-09 12:36:34 +0530

    final sdk

--- bmc.go
+++ bmc.go
@@ -1,31 +1,6 @@
 package oraclebmc_sdk
 
-import (
-	"bytes"
-	"crypto/sha256"
-	"encoding/json"
-	"fmt"
-	"github.com/99designs/httpsignatures-go"
-	"io"
-	"io/ioutil"
-	"net/http"
-)
-
-type oracle_config struct {
-	user                         string
-	tenancy                      string
-	fingerprint                  string
-	private_key                  string
-	core_endpoint                string
-	endpoint_blockstorage_api    string
-	endpoint_identity_api        string
-	endpoint_compute_api         string
-	endpoint_virtual_network_api string
-	endpoint_object_storage_api  string
-	verify_ssl                   bool
-	log_requests                 bool
-	additional_user_agent        string
-}
+import "fmt"
 
 func NewConfig(user string, tenancy string, fingerprint string, signing_key string) *oracle_config {
 	core_endpoint := "https://iaas.us-phoenix-1.oraclecloud.com/20160918"
@@ -52,50 +27,6 @@ type ComputeApi struct {
 	Config *oracle_config
 }
 
-type oracleRequest struct {
-	Url          string
-	Suffix       string
-	Method       string
-	Output       interface{}
-	OracleConfig *oracle_config
-	Params       map[string]string
-}
-
-func (orReq *oracleRequest) doReq() error {
-	var body io.Reader
-	if orReq.Params != nil && len(orReq.Params) > 0 {
-		val, err := json.Marshal(orReq.Params)
-		if err != nil {
-			return err
-		}
-		body = bytes.NewBuffer(val)
-	} else {
-		body = nil
-	}
-	req, err := http.NewRequest(orReq.Method, orReq.Url+orReq.Suffix, body)
-	if body == nil {
-		url := req.URL
-		q := url.Query()
-		for key, val := range orReq.Params {
-			q.Set(key, val)
-		}
-		url.RawQuery = q.Encode()
-
-	}
-	if err != nil {
-		return err
-	}
-	inject_headers(orReq.OracleConfig, req)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(orReq.Output)
-	return nil
-}
-
 func (computeApi *ComputeApi) GetInstance(instanceId string) (*Instance, error) {
 	suffix := fmt.Sprintf("/instances/%s", instanceId)
 
@@ -106,7 +37,8 @@ func (computeApi *ComputeApi) GetInstance(instanceId string) (*Instance, error) 
 		Suffix:       suffix,
 		Method:       "GET",
 		OracleConfig: computeApi.Config,
-		Params:       nil,
+		Body:         nil,
+		QueryParams:  nil,
 		Output:       output}
 	err := orReq.doReq()
 	if err != nil {
@@ -129,7 +61,8 @@ func (computeApi *ComputeApi) ListImages(compartment_id string) (*[]*Image, erro
 		Suffix:       suffix,
 		Method:       "GET",
 		OracleConfig: computeApi.Config,
-		Params:       nil,
+		QueryParams:  params,
+		Body:         nil,
 		Output:       output}
 
 	err := orReq.doReq()
@@ -140,30 +73,22 @@ func (computeApi *ComputeApi) ListImages(compartment_id string) (*[]*Image, erro
 	return output, nil
 }
 
-func inject_headers(oracleConfig *oracle_config, request *http.Request) {
-	var required_headers []string
-	request.Header.Set("host", request.URL.Host)
-	if request.Method == "POST" || request.Method == "PUT" {
+func (computeApi *ComputeApi) CreateInstance(launchInstanceInput *LaunchInstanceInput) (Instance, error) {
+	suffix := "/instances"
+	var instance Instance
+	output := &instance
+	orReq := oracleRequest{
+		Url:          computeApi.Config.core_endpoint,
+		Suffix:       suffix,
+		Method:       "POST",
+		OracleConfig: computeApi.Config,
+		Body:         launchInstanceInput,
+		QueryParams:  nil,
+		Output:       output}
 
-		required_headers = []string{
-			httpsignatures.RequestTarget,
-			"date",
-			"host",
-			"x-content-sha256",
-			"content-type",
-			"content-length"}
-
-		body, _ := ioutil.ReadAll(request.Body)
-		hash := sha256.New()
-		hash.Write([]byte(body))
-		content_body := hash.Sum(nil)
-
-		request.Header.Set("x-content-sha256", string(content_body))
-		request.Header.Set("content-length", string(len(body)))
-		request.Header.Set("content-type", "application/json")
-	} else {
-		required_headers = []string{httpsignatures.RequestTarget, "date", "host"}
+	err := orReq.doReq()
+	if err != nil {
+		return nil, err
 	}
-	signer := httpsignatures.NewSigner(httpsignatures.AlgorithmRsaSha256, required_headers...)
-	signer.AuthRequest(oracleConfig.getKey(), oracleConfig.private_key, request)
+	return output, nil
 }
--- models.go
+++ models.go
@@ -1,6 +1,8 @@
 package oraclebmc_sdk
 
-import "time"
+import (
+	"time"
+)
 
 type Image struct {
 	BaseImageId            string
@@ -24,4 +26,30 @@ type Instance struct {
 	Shape              string
 	TimeCreated        time.Time
 	Metadata           map[string]string
+}
+
+type LaunchInstanceInput struct {
+	AvailabilityDomain string
+	CompartmentId      string
+	DisplayName        string
+	ImageId            string
+	Metadata           map[string]string
+	Shape              map[string]string
+	SubnetId           map[string]string
+}
+
+type oracle_config struct {
+	user                         string
+	tenancy                      string
+	fingerprint                  string
+	private_key                  string
+	core_endpoint                string
+	endpoint_blockstorage_api    string
+	endpoint_identity_api        string
+	endpoint_compute_api         string
+	endpoint_virtual_network_api string
+	endpoint_object_storage_api  string
+	verify_ssl                   bool
+	log_requests                 bool
+	additional_user_agent        string
 }
